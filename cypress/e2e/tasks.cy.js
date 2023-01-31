d54ce283b61c3eeffdf747b841abb5f5f5087179
Bruno Teixeira <bruno.teixeira92@edu.pucrs.br>
2023-01-31 16:25:41 +0000

    Adding custom commands

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -4,22 +4,8 @@ describe('tasks', ()=> {
     
     it('should register a new task', ()=> {
         const creationTaskName = "Te amar de janeiro a janeiro"
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name:creationTaskName}
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
-        cy.visit('http://localhost:8080')
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(creationTaskName)
-        //button[contains(text(), "Create")]
-        cy.contains('button', 'Create')
-            .click()
-        /*cy.get('main div p')
-            .should('be.visible')
-            .should('have.text', 'Botar na tomada se acabar a pilha')*/
+        cy.removeTaskByName(creationTaskName)
+        cy.createTask(creationTaskName)
         cy.contains('main div p', creationTaskName)
             .should('be.visible')
     })
@@ -29,28 +15,17 @@ describe('tasks', ()=> {
             name: 'Tentar te encontrar tanto pra dizer meu amor tudo bem',
             is_done: false
         }
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: duplicatedTask.name}
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
-        cy.request({
-            url: 'http://localhost:3333/tasks',
-            method: 'POST',
-            body: duplicatedTask
-        }).then(response => {
-            expect(response.status).to.eq(201)
-        })
-        cy.visit('http://localhost:8080')
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(duplicatedTask.name)
-        cy.contains('button', 'Create')
-            .click()
+        cy.removeTaskByName(duplicatedTask.name)
+        cy.addTaskByAPI(duplicatedTask)
+        cy.createTask(duplicatedTask.name)
         cy.get('.swal2-html-container')
             .should('be.visible')
             .should('have.text', "Task already exists!")
+    })
+    
+    it('should display error message for empty field', () => {
+        cy.createTask()
+        cy.isRequired('This is a required field')
     })
 
 })

b0a904cef4a0d3635717ef1a4e5419ab78e12f78
Bruno Teixeira <bruno.teixeira92@edu.pucrs.br>
2023-01-31 13:38:10 +0000

    Implementing duplicated task test

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,24 +3,54 @@
 describe('tasks', ()=> {
     
     it('should register a new task', ()=> {
+        const creationTaskName = "Te amar de janeiro a janeiro"
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
-            body: {name:'Botar na tomada se acabar a pilha'}
+            body: {name:creationTaskName}
         }).then(response => {
             expect(response.status).to.eq(204)
         })
         cy.visit('http://localhost:8080')
         cy.get('input[placeholder="Add a new Task"]')
-            .type("Botar na tomada se acabar a pilha")
+            .type(creationTaskName)
         //button[contains(text(), "Create")]
         cy.contains('button', 'Create')
             .click()
         /*cy.get('main div p')
             .should('be.visible')
             .should('have.text', 'Botar na tomada se acabar a pilha')*/
-        cy.contains('main div p', 'Botar na tomada se acabar a pilha')
+        cy.contains('main div p', creationTaskName)
             .should('be.visible')
+    })
+
+    it('should not allow duplicated tasks', ()=> {
+        const duplicatedTask = {
+            name: 'Tentar te encontrar tanto pra dizer meu amor tudo bem',
+            is_done: false
+        }
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: duplicatedTask.name}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: duplicatedTask
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+        cy.visit('http://localhost:8080')
+        cy.get('input[placeholder="Add a new Task"]')
+            .type(duplicatedTask.name)
+        cy.contains('button', 'Create')
+            .click()
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', "Task already exists!")
     })
 
 })
